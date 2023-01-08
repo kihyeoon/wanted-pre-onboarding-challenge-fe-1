b95b572e34dd23ebd70da9a5b5c80b55035050c1
kihyeoon <bangion93@gmail.com>
2023-01-09 03:35:19 +0900

    ✨ feat: 토큰 보유시 리다이렉트 구현

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,7 +1,18 @@
 import AuthForm from "../components/AuthForm";
 import useLogIn from "../hooks/useLogIn";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function SignUp() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/", { replace: true });
+    }
+  }, [navigate]);
+
   const { mutate } = useLogIn();
 
   const handleLogIn = ({ email, password }) => {
--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -1,7 +1,18 @@
 import AuthForm from "../components/AuthForm";
 import useSignUp from "../hooks/useSignUp";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/", { replace: true });
+    }
+  }, [navigate]);
+
   const { mutate } = useSignUp();
 
   const handleLogIn = ({ email, password }) => {
